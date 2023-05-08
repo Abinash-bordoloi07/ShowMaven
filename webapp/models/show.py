abe7aac711984fc732412cd67285c85056760855
Abinash <iteratex.pseudocode@gmail.com>
2023-05-08 17:49:39 +0530

    show model added

---
+++ webapp/models/show.py
@@ -0,0 +1,17 @@
+from webapp import db
+
+
+class Show(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True, unique=True)
+    rating = db.Column(db.Float)
+    tags = db.Column(db.String(120))
+    ticket_price = db.Column(db.Float)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'))
+
+    def __repr__(self):
+        return f'<Show {self.name}>'
+
+
+class SearchForm(FlaskForm):
+    search = StringField('Search', validators=[DataRequired()])
